cd01281dc57bcbc924a3fdcdff65aac9508e5e98
William Dias <smdwilliam89@gmail.com>
2026-01-27 07:19:39 +0000

    feat: Adding the Footer to the project.

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Manrope } from 'next/font/google';
 import './globals.css';
 
 import Navbar from '@/components/layout/Navbar';
+import Footer from '@/components/layout/Footer';
 
 const manrope = Manrope({
     subsets: ['latin'],
@@ -25,6 +26,7 @@ export default function RootLayout({
             <body className={manrope.className} suppressHydrationWarning={true}>
                 <Navbar />
                 {children}
+                <Footer />
             </body>
         </html>
     );
